8e5f248a1c279283476787b76558ad341712994d
CindyvdVries <cindyvdvries@gmail.com>
2024-09-19 11:24:17 +0200

    first setup + 3d env

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-main>
+      <TresCanvas window-size clear-color="#82DBC5">
+        <TresPerspectiveCamera :position="[9, 9, 9]" />
+        <OrbitControls />
+        <Suspense>
+          <Environment
+            background
+            :files="environmentFiles"
+            path="https://raw.githubusercontent.com/Tresjs/assets/main/textures/environmentMap"
+          />
+        </Suspense>
+      </TresCanvas>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import { TresCanvas } from '@tresjs/core'
+import { OrbitControls, Environment } from '@tresjs/cientos';
+
+export default {
+  name: 'App',
+
+  components: {
+    TresCanvas,
+    OrbitControls,
+    Environment
+  },
+
+  data: () => ({
+    environmentFiles:  ['/px.jpg', '/nx.jpg', '/py.jpg', '/ny.jpg', '/pz.jpg', '/nz.jpg']
+  }),
+
+  mounted () {
+    // this.scene = useGLTF('https://raw.githubusercontent.com/Tresjs/assets/main/models/gltf/realistic-pokeball/scene.gltf', { draco: true })
+  }
+}
+</script>
