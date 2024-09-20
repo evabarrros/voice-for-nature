86941d3afbf781eecd1a5d7f749b23113687b9a5
CindyvdVries <cindyvdvries@gmail.com>
2024-09-20 09:49:57 +0200

    setup stories and video

--- src/components/StoriesComponent.vue
+++ src/components/StoriesComponent.vue
@@ -4,23 +4,37 @@
     :debug="true"
     :offset="0.5"
   >
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quod blanditiis atque est minima incidunt ab impedit. Natus, soluta consectetur culpa non sint pariatur repellat quia, dolorem quo doloribus excepturi!
+  <v-card class="pa-3 ma-3" outlined color="rgba(0, 0, 0, 0.5)">
+    <h2>
+      Deltapond
+    </h2>
+    <div class="history" md="12">
+      <div class="history-markdown" v-html="content" />
     </div>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quod blanditiis atque est minima incidunt ab impedit. Natus, soluta consectetur culpa non sint pariatur repellat quia, dolorem quo doloribus excepturi!
+    <div class="video-wrap">
+    <video controls="true">
+      <source src="./stories/history/timelapse.mp4" type="video/mp4">
+    </video>
+  </div>
+    <div class="history" md="12">
+      <div class="history-markdown" v-html="content2" />
     </div>
+
+
+  </v-card>
   </Scrollama>
 </template>
-
 <script>
+import content from './stories/history/text.md'
+import content2 from './stories/history/text2.md'
+
 export default {
   name: 'BackgroundViewer',
-
-  data: () => ({
-  }),
-
-  mounted () {
+  data () {
+    return {
+      content,
+      content2,
+    }
   }
 }
 </script>
