a8b00aa3a649ab652a54405fc19de4d003b7b52a
CindyvdVries <cindyvdvries@gmail.com>
2024-09-24 20:52:19 +0200

    fix 3d view

--- src/components/BackgroundViewer.vue
+++ src/components/BackgroundViewer.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <TresCanvas window-size clear-color="#82DBC5">
-      <TresPerspectiveCamera ref="camera"/>
-      <OrbitControls :enablePan="false" :enableZoom="false"/>
+    <TresCanvas window-size clear-color="#82DBC5" ref="canvas">
+      <TresPerspectiveCamera visible ref="camera" :position="currentLocation.camera" :look-at="[150, 0, 100]"/>
+      <OrbitControls :target="currentLocation.position" :enablePan="false" :enableZoom="false"/>
+      <TresGridHelper />
       <Suspense>
-        <Environment
-          background
-          :files="locations[currentLocation].image"
+        <Environment 
+          ref = "env"
+          :background="true"
+          :files="`./360photos/${currentLocation.id}.hdr`"
         />
       </Suspense>
-      <TresScene>
+      <TresScene
+        ref="scene">
         <TresDirectionalLight
           :position="[0, 8, 4]"
           :intensity="0.2"
@@ -19,8 +22,9 @@
           v-for="location in locations"
           :key="location.id"
           :position="location.position"
-          :visible="location.id !== currentLocation"
-          @click="currentLocation = location.id"
+          :ref="location.id"
+          :visible="location.id !== currentId"
+          @click="onClick(location.id)"
           @pointer-enter="onPointerEnter"
           @pointer-leave="onPointerLeave"
           >
@@ -45,28 +49,55 @@ export default {
   },
   data () {
     return {
-      currentLocation: "loc1",
-      locations: {
-        loc1: {
-          id: "loc1",
-          image: "./deltapond2.hdr",
-          position: [0, 0, 0]
-        },
-        loc2: {
-          id: "loc2",
-          image: "./deltapond1.hdr",
-          position: [-200, 0, 100]
-        },
-        loc3: {
-          id: "loc3",
-          image: "./deltapond3.hdr",
-          position: [-100, 0, 200]
+      currentId: "loc7_other",
+      locations: [
+        {
+          id: "loc1_road",
+          position: [40, 0, 0],
+          camera: [39, 0, -1],
+          rotation: 1.6
+        }, {
+          id: "loc2_pavilion",
+          position: [0, 0, 90],
+          camera: [-1, 0, 90],
+          rotation: 3.0
+        }, {
+          id: "loc3_tetra",
+          position: [30, 0, 180],
+          camera: [29, 0, 181],
+          rotation: 3.8
+        }, {
+          id: "loc4_farside",
+          position: [110, 0, 250],
+          camera: [110, 0, 251],
+          rotation: 4.3
+        }, {
+          id: "loc5_bench",
+          position: [160, 0, 190],
+          camera: [161, 0, 190],
+          rotation: 5.7
+        }, {
+          id: "loc6_sluice",
+          position: [160, 0, 80],
+          camera: [161, 0, 80],
+          rotation: 0.3
+        }, {
+          id: "loc7_other",
+          position: [120, 0, 40],
+          camera: [121, 0, 39],
+          rotation: 1.55
         }
-      }
+      ]
+    }
+  },
+  computed: {
+    currentLocation () {
+      return this.locations.find(loc => loc.id === this.currentId)
     }
   },
   mounted () {
-    console.log(this.$refs.camera)
+    console.log(this.$refs)
+    this.rotateEnvironment(this.currentLocation.rotation)
   },
   methods: {
     onPointerEnter(ev) {
@@ -79,11 +110,13 @@ export default {
         ev.object.material.color.set('#efefef')
       }
     },
-    onClick(ev) {
-      console.log(ev)
-      if (ev) {
-        ev.object.material.color.set('#008080');
-      }
+    onClick(locationId) {
+      console.log(locationId)
+      this.currentId = locationId
+      this.rotateEnvironment(this.currentLocation.rotation)
+    },
+    rotateEnvironment(rad) {
+      this.$refs.scene.parent.backgroundRotation.y = rad
     }
   }
 }
